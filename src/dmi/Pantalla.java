2a3dca920e7f8d7fb4de2381c693c58105db2419
Ivan Izquierdo Jimenez <tremeempresa@gmail.com>
2022-04-17 15:25:09 +0200

    Separado

--- src/dmi/Pantalla.java
+++ src/dmi/Pantalla.java
@@ -5,6 +5,7 @@ import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 import javax.swing.Timer;
 import javax.swing.border.Border;
@@ -28,6 +29,7 @@ public class Pantalla extends JFrame{
 	JLabel symb;
 	JLabel dr = new JLabel();
 	Border c;
+	JPanel panel = new JPanel();
 	Icon[] iconmodo;
 	float scale = /*1.3f*/ 1f;
 	public static Archivo arc = new Archivo();
@@ -40,12 +42,14 @@ public class Pantalla extends JFrame{
 		 setSize(getScale(465), getScale(550));
 		 setVisible(true);
 		 setResizable(false);
+		 //setUndecorated(true);
 		 //setExtendedState(JFrame.MAXIMIZED_BOTH);
 		 getContentPane().setBackground(Color.BLACK);
 		 setDefaultCloseOperation(EXIT_ON_CLOSE);
 		 setLayout(null);
-		 //setUndecorated(true);
-		 vel = new Velocidad(this);
+		 PanelDisplay();
+		 getContentPane().add(panel);
+		 /*vel = new Velocidad(this);
 		 add(vel);
 		 vel.setBounds(0, 0, getScale(450), getScale(350));
 		 for(int i=0; i<=v_max; i+=10)
@@ -104,7 +108,7 @@ public class Pantalla extends JFrame{
 		 JLabel vigil = new JLabel();
 		 vigil.setIcon(new ImageIcon(getClass().getResource("../dmi/vigilancia.JPG")));
 		 add(vigil);
-		 vigil.setBounds(getScale(10), getScale(10), getScale(50), getScale(44));
+		 vigil.setBounds(getScale(10), getScale(10), getScale(50), getScale(44));*/
 	}
 	public void updateReal(float curr)
 	{
@@ -148,6 +152,67 @@ public class Pantalla extends JFrame{
 			Thread.sleep(350);
 		}
 		return true;
+	}
+	public void PanelDisplay() 
+	{
+		vel = new Velocidad(this);
+		 add(vel);
+		 vel.setBounds(0, 0, getScale(450), getScale(350));
+		 for(int i=0; i<=v_max; i+=10)
+		 {
+			 JLabel j = new JLabel(Integer.toString(i));
+			 j.setFont(new Font("Arial", Font.PLAIN, getScale(20)));
+			 j.setForeground(Color.YELLOW);
+			 j.setHorizontalAlignment(SwingConstants.CENTER);
+			 int r = 125;
+			 if(i<100) r = 130;
+			 if(i==0) r = 135;
+			 double cx = centx - r*Math.cos(Math.toRadians(AngIni+i/(float)v_max*Ang));
+			 double cy = centy - r*Math.sin(Math.toRadians(AngIni+i/(float)v_max*Ang));
+			 add(j);
+			 j.setBounds(getScale(cx-25), getScale(cy-10), getScale(50), getScale(20));
+		 }
+		 spd = new JLabel(Integer.toString(Math.round(v_act)));
+		 spd.setFont(new Font("Arial", Font.PLAIN, getScale(26)));
+		 spd.setForeground(Color.GREEN);
+		 spd.setHorizontalAlignment(SwingConstants.RIGHT);
+		 Border b = BorderFactory.createLineBorder(Color.RED);
+		 spd.setBorder(b);
+		 add(spd);
+		 spd.setBounds(getScale(centx-30), getScale(centy-70), getScale(60), getScale(30));
+		 
+		 JLabel unit = new JLabel("km/h");
+		 unit.setFont(new Font("Arial", Font.PLAIN, getScale(20)));
+		 unit.setForeground(Color.BLUE);
+		 unit.setHorizontalAlignment(SwingConstants.CENTER);
+		 add(unit);
+		 unit.setBounds(getScale(centx-40), getScale(centy-40), getScale(80), getScale(30));
+		 
+		 modo = new JLabel();
+		 modo.setFont(new Font("Arial", Font.PLAIN, getScale(14)));
+		 modo.setForeground(Color.YELLOW);
+		 modo.setHorizontalAlignment(SwingConstants.CENTER);
+		 modo.setBorder(b);
+		 updateModo(1);
+		 add(modo);
+		 modo.setBounds(getScale(centx-60), getScale(centy), getScale(120), getScale(30));
+		 
+		 Border sm = BorderFactory.createLineBorder(Color.WHITE);
+		 JLabel symb = new JLabel();
+		 symb.setBorder(sm);
+		 add(symb);
+		 symb.setBounds(getScale(300), getScale(300), getScale(32), getScale(32));
+		 
+		 iconmodo = new ImageIcon[3];
+		 iconmodo[0] = new ImageIcon(getClass().getResource("/dmi/MO_13.jpg"));
+		 iconmodo[1] = new ImageIcon(getClass().getResource("/dmi/MO_16.jpg"));
+		 iconmodo[2] = new ImageIcon(getClass().getResource("/dmi/MO_19.jpg"));
+		 add(dr);
+		 
+		 JLabel vigil = new JLabel();
+		 vigil.setIcon(new ImageIcon(getClass().getResource("../dmi/vigilancia.JPG")));
+		 add(vigil);
+		 vigil.setBounds(getScale(10), getScale(10), getScale(50), getScale(44));
 	}
 	public static void main(String[] args) throws InterruptedException
 	{
